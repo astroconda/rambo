b3e82911587c49863a4dab105aa139f61e53945f
Matt Rendina <mrendina@stsci.edu>
2017-05-23 11:53:21 -0400

    Turning into a package

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+version = {}
+with open("rambo/_version.py") as fp:
+        exec(fp.read(), version)
+        # later on use: version['__version__']
+
+setup(
+    name='rambo',
+    version=version['__version__'],
+    author='Matt Rendina',
+    author_email='mrendina@stsci.edu',
+    description='Recipe Analyzer and Multi-package Build Optimizer',
+    url='https://github.com/astroconda/rambo',
+    license='GPLv2',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Build Tools',
+    ],
+    packages=find_packages(),
+    package_data={'': ['README.md', 'LICENSE.txt']},
+    entry_points = {
+        'console_scripts': ['rambo=rambo.rambo:main'],
+    }
+)
